4b1dcb8ecf8a4ead04b2a05f9e9d9678cefc515e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-10 17:22:55 -0300

    add campo city, state e country para users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
