e516cd16dd37786dc5995469288411536badd47c
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-12 16:28:54 -0300

    route for show user

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/create', [HomeController::class, 'create']);
-Route::post('/create', [HomeController::class, 'store']);
-Route::get('/about', [HomeController::class, 'about']);
+Route::get('/', [UserController::class, 'index'])->name('home');
+Route::get('/create', [UserController::class, 'create']);
+Route::get('/show/{id}', [UserController::class, 'show'])->name('show');
+Route::post('/create', [UserController::class, 'store']);
+Route::get('/about', [UserController::class, 'about']);
